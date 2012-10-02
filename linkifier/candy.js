b8f7d091c5e4f1e33f853fd58f587e68567d3dc1
Jack Lawson <ajacksified@gmail.com>
2012-10-01 22:25:20 -0700

    oh yeah, external links are good

--- linkifier/candy.js
+++ linkifier/candy.js
@@ -4,7 +4,7 @@ CandyShop.Linkifier = (function(self, Candy, $) {
   self.init = function(options) {
     $(Candy.View.Pane).bind('candy:view.message.beforeShow', function(e, args) {
       var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-      args.message = args.message.replace(exp,"<a href='$1'>$1</a>");
+      args.message = args.message.replace(exp,"<a href='$1' target='_blank' ref='nofollow'>$1</a>");
     });
   };
 
